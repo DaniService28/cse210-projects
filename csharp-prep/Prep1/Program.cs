c2dcf18068f4cf38fb00a2b0441f0f119d2ac91e
DaniService28 <her23048@byui.edu>
2025-09-22 17:42:53 -0600

    Changed something

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        // Declare variable
+        string firstName = "Bob";
+        Console.WriteLine($"Hello {firstName}");
+
+        // Input lines
+        Console.Write("What's your name?");
+        string userName = Console.ReadLine();
+
+        // numbers
+        Console.WriteLine("How old are you?");
+        int age = int.Parse(Console.ReadLine());
+
+        //decimals numbers
+        double x = 5.5;
+
+        //float numbers
+        float y = 5.5f;
+
+        // booleans
+        bool isDone = false;
+
+        if (isDone)
+        {
+            Console.WriteLine("All done");
+        }
     }
 }
