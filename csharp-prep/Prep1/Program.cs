d166ed1f91ea763fbb3d63d762aaff3d2a2a758c
DaniService28 <her23048@byui.edu>
2025-09-22 23:02:43 -0600

    Assigment 2 on week 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
@@ -31,12 +32,49 @@ class Program
         // {
         //     Console.WriteLine("All done");
         // }
-        Console.Write("What is your first name: ");
-        string first_name = Console.ReadLine();
+        // Console.Write("What is your first name: ");
+        // string first_name = Console.ReadLine();
 
-        Console.Write("What is your last name: ");
-        string last_name = Console.ReadLine();
+        // Console.Write("What is your last name: ");
+        // string last_name = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
+        // Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
+
+        Console.Write("What's your grade percentage?: ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (letter == "C" || letter == "B" || letter == "C")
+        {
+            Console.WriteLine($"Your grade is {letter}");
+            Console.WriteLine($"Congratulations you passed the course");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is {letter}");
+            Console.WriteLine($"Try harder next time");
+        }
     }
 }
