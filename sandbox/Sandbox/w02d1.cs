3bc2ec8bed58267159ec3ce51151fffc878407f7
DaniService28 <her23048@byui.edu>
2025-09-25 10:25:46 -0600

    asd

---
+++ sandbox/Sandbox/w02d1.cs
@@ -0,0 +1,38 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //foreach
+        List<string> fruits = new List<string>() { "Apples", "Banana", "Cherry" };
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+
+        //for loops
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.WriteLine($"i: {i}");
+        }
+
+        // while loop
+        Console.Write("Make a choice (y or n): ");
+        string value = Console.ReadLine().ToUpper();
+
+        while (value != "Y" && value != "N")
+        {
+            Console.WriteLine("Chose y or n");
+            value = Console.ReadLine().ToUpper();
+        }
+
+        // do while loop
+        do
+        {
+            Console.Write("Enter y or n");
+            Console.ReadLine().ToUpper();
+
+        } while (value != "Y" && value != "N");
+    }
+}
